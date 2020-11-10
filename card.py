9162bf6539a696c2cdec8b36436fd771d9d059a6
NrdyBhu1 <61649925+NrdyBhu1@users.noreply.github.com>
2020-11-10 16:55:06 +0530

    Update card.py

--- card.py
+++ card.py
@@ -4,28 +4,28 @@ from io import BytesIO
 
 font = ImageFont.truetype("OpenSans-Semibold.ttf", size=25)
 
-def get_green_card(name, img, status, activity):
-    g_card = get_card("green", name, img, status, activity)
+def get_green_card(name, img, status, activity, nick_name="None"):
+    g_card = get_card("green", name, img, status, activity, nick_name)
     return g_card
 
 
-def get_blue_card(name, img, status, activity):
-    b_card = get_card("blue", name, img, status, activity)
+def get_blue_card(name, img, status, activity, nick_name="None"):
+    b_card = get_card("blue", name, img, status, activity, nick_name)
     return b_card
 
 
-def get_purp_card(name, img, status, activity):
-    p_card = get_card("purp", name, img, status, activity)
+def get_purp_card(name, img, status, activity, nick_name="None"):
+    p_card = get_card("purp", name, img, status, activity, nick_name)
     return p_card
 
 
-def get_yellow_card(name, img, status, activity):
-    y_card = get_card("yellow", name, img, status, activity)
+def get_yellow_card(name, img, status, activity, nick_name="None"):
+    y_card = get_card("yellow", name, img, status, activity, nick_name)
     return y_card
 
 
-def get_white_card(name, img, status, activity):
-    w_card = get_card("white", name, img, status, activity)
+def get_white_card(name, img, status, activity, nick_name="None"):
+    w_card = get_card("white", name, img, status, activity, nick_name)
     return w_card
 
 def get_card(card_name, name, img, status, activity, nick_name="None"):
@@ -34,7 +34,7 @@ def get_card(card_name, name, img, status, activity, nick_name="None"):
     draw.text((50, 10), str(name), font=font, fill='black')
     draw.text((50, 190), "status:"+str(status).lower(), font=font, fill='black')
     draw.text((50, 225), "activity:"+str(activity).lower(), font=font, fill='black')
-    draw.text((50, 225), "nick:"+str(nick_name).lower(), font=font, fill='black')
+    draw.text((50, 255), "nick:"+str(nick_name).lower(), font=font, fill='black')
     response = requests.get(img)
     avatar = Image.open(BytesIO(response.content))#, Image.ANTIALIAS)
     avatar = avatar.resize((130, 130))
